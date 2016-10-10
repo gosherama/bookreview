cc13f429ca170c44877847e002939659e378793c
gosher <macciak@gmail.com>
2016-10-10 20:52:40 +0000

    simple form and partial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,17 @@
 class BooksController < ApplicationController
+    def index
+    end
+    
+    def new
+        @book = Book.new
+    end
+    
+    def create
+        @book = Book.new(book_params)
+    end
+    
+    private 
+        def book_params
+            params.require(:book).permit(:title, :description, :author)
+        end
 end
