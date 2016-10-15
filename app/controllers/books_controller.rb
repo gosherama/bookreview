8b35cfa53e30ff93fd5943a15bdb37c0f2aa6e52
gosher <macciak@gmail.com>
2016-10-15 16:16:04 +0000

    basic CRUD completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,22 @@ class BooksController < ApplicationController
         end
     end
     
+    def edit
+    end
+    
+    def update
+        if @book.update(book_params)
+            redirect_to book_path(@book)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @book.destroy
+        redirect_to root_path
+    end
+    
     private 
         def book_params
             params.require(:book).permit(:title, :description, :author)
