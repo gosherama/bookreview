7b1f7321e1b5bab4b61e42ef8250de2db3c70ba8
gosher <macciak@gmail.com>
2016-10-17 14:58:10 +0000

    Add categories

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,10 +10,12 @@ class BooksController < ApplicationController
     
     def new
         @book = current_user.books.build
+        @categories = Category.all.map { |c| [c.name, c.id] }
     end
     
     def create
         @book = current_user.books.build(book_params)
+        @book.category_id = params[:category_id]
         
         if @book.save
             redirect_to root_path
@@ -40,7 +42,7 @@ class BooksController < ApplicationController
     
     private 
         def book_params
-            params.require(:book).permit(:title, :description, :author)
+            params.require(:book).permit(:title, :description, :author, :category_id)
         end
         def find_book
             @book = Book.find(params[:id])
